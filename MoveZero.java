cce2af43a12e0b51cceb025b719efa1913802ce3
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-10-18 21:07:49 +0530

    Update MoveZero.java

--- MoveZero.java
+++ MoveZero.java
@@ -25,3 +25,38 @@ for(int i=0;i<n;i++){
 }
 }
 }
+
+
+
+
+
+
+//MOVE ALL ZERO IN RIGHT SIDE OF AN ARRAY
+//Using traveral swaping in single case 
+import java.util.*;
+class Main{
+public static void moveZero(int []arr,int n){
+int count=0;
+int temp;
+for(int i=0;i<n;i++){
+  if(arr[i]>0){
+   temp=arr[count];//swap
+   arr[count]=arr[i];
+   arr[i]=temp;
+   count=count+1; 
+}
+}
+}
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<n;i++){
+  arr[i]=sc.nextInt();
+}
+moveZero(arr,n);
+for(int i=0;i<n;i++){
+  System.out.print(arr[i]+" ");
+}
+}
+}
